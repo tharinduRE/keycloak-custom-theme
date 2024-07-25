10451b4c2be5d5cd967ff2bb84e345e58bb2ef00
Slava Vashchonak <s.vashchonak@katanox.com>
2024-07-25 15:20:30 +0200

    test config

--- publish-release.sh
+++ publish-release.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 commit_message=$(git log --format=oneline --pretty=format:%s -n 1 $CIRCLE_SHA1)
-latest_tag=${gh release view --json tagName -q .tagName}
+latest_tag=$(gh release view --json tagName -q .tagName)
 
 echo "Commit message: $commit_message" 
 echo "Latest tag: $latest_tag"
-
-
-#!/bin/bash
 
 # Function to increment version
 increment_version() {
@@ -28,11 +25,17 @@ increment_version() {
   fi
 
   # Join version parts and return new version
-  echo "v$major.$minor.$patch"
+  new_version="v$major.$minor.$patch"
+
+  echo "$new_version"
 }
 
 # Get new version
 new_version=$(increment_version "$latest_tag" "$commit_message")
+is_master=false
 
-# Output new version
-echo "New version: $new_version"
+if [[ $CIRCLE_BRANCH == "master" ]]; then
+    is_master=true
+fi
+
+gh release create $new_version ./providers/*.jar --latest=$is_master --title=$new_version --notes=$commit_message
